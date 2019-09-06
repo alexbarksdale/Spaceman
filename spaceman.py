0963b884b292c7b6898ce10920379b5a762c317c
alexbarksdale <ale.barksdale@gmail.com>
2019-09-05 18:51:55 -0700

    implemented regex to user_input

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,10 @@
 import random
 import os
+import re
+from colorama import Fore, Back, Style
+
+
+# TODO: Remove line 65 later
 
 # Acknowledgement: load_word() is from starter code
 
@@ -14,61 +19,87 @@ def load_word():
     return secret_word
 
 
+# Stores the correct words and wrong words
 correct_list = []
-guess_list = []
+incorrect_list = []
+
+# user_input uses REGEX to look through the user's input and checks to see if there are only letters (A-Z)
 
 
 def user_input():
     while True:
         guess = input('Guess a letter: ').lower()
 
-        if len(guess) > 1:
-            print('Please use single letters only.')
-        elif guess.isnumeric():
-            print('Please guess using letters only.')
+        if len(guess) <= 1 and guess.isalpha():
+            if re.match(r'^[a-zA-Z]*$', guess):
+                return guess
         else:
-            return guess
-
-
-guessCount = 0
+            print(Fore.RED + 'Please use single letters only.' + Fore.RESET)
 
 
 def guess_checker(guess, secret_word):
-    global guessCount
-    if guessCount <= 6:
-        if guess in guess_list:
-            print('\nYou have alread guessed that letter.')
-        elif guess in secret_word:
-            correct_list.append(guess)
-            print('\nYou guessed a letter correctly!')
-        else:
-            guess_list.append(guess)
-            print('\nYou guessed incorrectly.')
-            guessCount += 1
-
-        print('Guess test: ' + str(guessCount))
-
+    if guess in incorrect_list:
+        print('\nYou have alread guessed that letter.')
+    elif guess in secret_word:
+        correct_list.append(guess)
+        print('\nYou guessed a letter' + Fore.GREEN + ' correctly!' + Fore.RESET)
     else:
-        print('You ran out of guesses!')
+        incorrect_list.append(guess)
+        print('\nYou guessed' + Fore.RED + ' incorrectly.' + Fore.RESET)
+
+
+"""
+Line 62: Checks to see if the user has guessed less than 7 times. Returns False if they lost.
+Line 64: Loops through the letters of the secret_word
+Line 65-67: Checks to see if the player won. If the letter (i) is not in the correct_list the player hasn't won yet, so 
+once the user has all of the letters in the correct_list it will loop out and return True. True = won
+"""
+
+
+def win_checker(secret_word):
+    if len(incorrect_list) >= 7:
+        return False
+    for i in secret_word:
+        if i not in correct_list:
+            return 'Player hasn\'t won yet'
+    return True
+
+
+"""
+Line 82: Loops through the letters in the secret_word
+Line 83: If a letter is in correct_list it will replace the spot with the letter (i).
+Line 86: If there isn't a correct letter it will print '_' in position of the secret_word
+"""
 
 
 def spaceman(secret_word):
     while True:
-        print('-----------------------------------------')
+        print('------------------------------------------')
         print(secret_word)
         print('\nCorrect letters: ')
         for i in secret_word:
             if i in correct_list:
+                # end=' ' Appends a space after the print statement
                 print(i, end=' ')
             else:
                 print('_', end=' ')
         print('\n\nIncorrect letters: ')
-        for i in guess_list:
-            print(i, end=' ')  # end='' Appends a space
+        for i in incorrect_list:
+            print(i, end=' ')
         print('\n-----------------------------------------')
 
         guess = user_input()
         guess_checker(guess, secret_word)
+        win = win_checker(secret_word)
+
+        if win == False:
+            print(
+                Fore.RED + f'You lost! The word was: {secret_word}' + Fore.RESET)
+            break
+        elif win == True:
+            print(
+                Fore.GREEN + f'You won! The word was: {secret_word}' + Fore.RESET)
+            break
 
 
 spaceman(load_word())
